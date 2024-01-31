ab588ae4cf18aac4416cac9b295d7fadabe07972
Himanshu <himanshutariyal62@gmail.com>
2024-01-31 20:01:03 +0530

    change 3

--- src/modules/db/mongoose.ts
+++ src/modules/db/mongoose.ts
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 function connectDb() {
     try {
         const mongodbUri = process.env.mongodb_uri;
-        mongoose.connect(mongodbUri, {
-            useNewUrlParser: true,
-            // useFindAndModify: false
-        }).then(()=>console.log("database connection successfull"))
+        mongoose.connect(mongodbUri).then(()=>console.log("database connection successfull"))
         mongoose.set('debug', true);
     } catch (error) {
         console.log(error);
